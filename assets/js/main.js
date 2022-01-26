3611044cba2c485d138a8b9cee3f6e5457d38839
Kingori Anderson <96972017+kingorianderson@users.noreply.github.com>
2022-01-26 01:14:57 -0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*** CLOCK ***/
+const hour = document.getElementById('clock-hour'),
+      minutes = document.getElementById('clock-minutes'),
+      seconds = document.getElementById('clock-seconds')
+
+const clock = () =>{
+    let date = new Date()
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6
+        
+    // We add a rotation to the elements
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    minutes.style.transform = `rotateZ(${mm}deg)`
+    seconds.style.transform = `rotateZ(${ss}deg)`
+}
+setInterval(clock, 1000) // 1000 = 1s
+
